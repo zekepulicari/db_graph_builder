b98d9c4f377500b133173abdc6b6a2906d3af887
Kkkihon <kihonlinux@kihon.>
2025-09-13 03:33:41 +0000

    fix code

--- src/01_merging_dfs.R
+++ src/01_merging_dfs.R
@@ -1,6 +1,14 @@
+setwd("C:/Users/")  # Set wd here
+
+# Loading packages
 library(dplyr)
-setwd("C:/Users/")
-t1<-read.table("Filename.csv", sep="\t", header=TRUE)
-t2<-read.table("Filename.csv", sep="\t", header=TRUE)
-t3<-merge (t1, t2, by="Order")
-write.table(t3,"t4.csv",sep=",",quote=FALSE, row.names=FALSE)
+
+# Reading csv files
+t1 <- read.table("table_1.csv", sep= "\t", header= TRUE)
+t2 <- read.table("table_2.csv", sep= "\t", header= TRUE)
+
+# Merging dataframes
+t3 <- merge (t1, t2, by= "Order")
+
+# Writing merged dataframe to csv file
+write.table(t3, "t4.csv", sep= ",", quote= FALSE, row.names= FALSE)
--- src/02_corr_analysis.R
+++ src/02_corr_analysis.R
@@ -1,20 +1,37 @@
+# Set wd
+setwd("C:/Users/")
+
+# Loading packages
 library(Hmisc)
 library(reshape)
-setwd("C:/Users/")
-FQ_enzim_agras_hong_func_CLASIF <- read.delim("Filename.csv", row.names=1)
-tabAN<-FQ_enzim_agras_hong_func_CLASIF[(35:50),]
-tabAN<-as.matrix(tabAN)
-corrAN<-rcorr(tabAN, type="pearson")
-corrANp<-corrAN$P
-corrANp[upper.tri(corrANp)]<- NA
-longR<-melt(corrANr, id.vars="row.names")
-names(longR) <- c("funcion1","funcion2", "R")
-longP<-melt(corrANp, id.vars="row.names")
-names(longP) <- c("funcion1","funcion2", "P")
+
+# Reading csv file
+FQ_enzim_agras_hong_func_CLASIF <- read.delim("final_table.csv", row.names= 1)
+
+# Subset the df
+tabAN <- FQ_enzim_agras_hong_func_CLASIF[(35:50), ]
+tabAN <- as.matrix(tabAN)
+
+# Correlation analysis
+corrAN <- rcorr(tabAN, type= "pearson")
+corrANp <- corrAN$P
+corrANp[upper.tri(corrANp)] <- NA
+
+# Reshaping dataframes
+longR <- melt(corrANr, id.vars= "row.names")
+names(longR) <- c("funcion1", "funcion2", "R")
+longP <- melt(corrANp, id.vars="row.names")
+names(longP) <- c("funcion1", "funcion2", "P")
 longRP<-cbind(longR,longP$P)
-names(longRP) <- c("funcion1","funcion2","R","P")
-corr.imp.7<-longRP[which(abs(longRP$R)>0.7),]
+names(longRP) <- c("funcion1", "funcion2", "R", "P")
+
+# Subsetting significant correlations
+corr.imp.7 <- longRP[which(abs(longRP$R) > 0.7), ]
 fix(corr.imp.7)
-corr.imp.7.nNA<-corr.imp.7[complete.cases(corr.imp.7),]
+
+# Removing NAs
+corr.imp.7.nNA <- corr.imp.7[complete.cases(corr.imp.7), ]
 fix(corr.imp.7.nNA)
-write.table(corr.imp.7.nNA,"Filename.csv",row.names=FALSE,sep="\t",quote=FALSE)
+
+# Writing to csv file
+write.table(corr.imp.7.nNA, "correlation_07.csv", row.names=FALSE,sep="\t", quote=FALSE)
